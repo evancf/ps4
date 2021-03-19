564044d8b326aa0f5ea8aadedbee5f16cf5608b0
Evan Fricke <evanfricke@gmail.com>
2021-03-18 18:09:13 -0600

    Adding problem set instructions

---
+++ ps4_instructions.R
@@ -0,0 +1,77 @@
+# EEB 338/538 Analysis and Visualization of Biological Data
+# Problem Set 4
+
+#1a Load the iris data from R's datasets
+
+#b Extract the observations of species "setosa" into their own dataframe; repeat for "virginica"
+
+#c Examine histograms of sepal length and sepal width for setosa
+
+#d Test whether we can reject the null hypothesis that setosa sepal length and sepal width are each normally distributed 
+
+# e TRUE or FALSE? Can we conclude that setosa sepal length and sepal length are normally distributed?
+
+#f Why?
+
+
+#2a Model setosa sepal width as a constant
+
+#b How else can the value of the intercept be estimated?
+
+
+#3a Now model petal lengths as a function of sepal length in a model called m
+
+#b Examine a summary of the model
+
+#c What proportion of the variance is explained by the model?
+
+#d TRUE or FALSE? Does this model outperform a model without any covariates?
+
+#e TRUE or FALSE? Is the effect of sepal length significant at alpha = 0.1?
+
+#f TRUE or FALSE? Is the effect of sepal length at alpha = 0.05?
+
+#g TRUE or FALSE? Is the effect of sepal length at alpha = 0.01?
+
+
+#4a What is the null expectation for the intercept estimate?
+
+#b Based on the p-value, do we reject the null hypothesis?
+
+
+#5a Present results of the model in the form of a scatterplot of Iris setosa sepal width ~ sepal length
+
+#b Use purple filled triangles for points
+
+#c Change the axis labels to more readable text
+
+#d Add a regression line of best fit that is purple and dashed.
+
+#e Add text to the upper left corner displaying the coefficient of variation (i.e., proportion of variance explained by model)
+
+#f Add text to the upper left corder displaying the significant value of the beta coefficient
+
+
+#6a Visually examine the relationship between Iris virginica petal length and petal width
+
+#b Model the mean of Iris virginica petal width as a constant and examine a summary of the model
+
+#c Now model Iris virginica pedal width as a function of petal length
+
+#d To what extent does the model with petal length as a predictor explain the variance in the data?
+
+#e TRUE or FALSE? Would you conclude that more simple model of petal width as a constant is a better model when alpha = 0.05?
+
+#f What information did you use to make your decision?
+
+#g Add the regression line of best fit to the plot of the relationship between petal width and petal length
+
+
+#7a Check for homoscedasticity of errors
+
+#b Is this assumption met? Why or why not?
+
+#c Check assumptions of additivity and linearity
+
+#d Is this assumption met? Why or why not?
+
